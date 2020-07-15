71d64a0ea5a17f4c5a93046ce5c33db28017c423
LG <384446489@qq.com>
2020-07-15 21:59:49 +0800

    modify

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
-#include "mainwindow.h"
+﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    qDebug()<<"ddd";
 }
 
 MainWindow::~MainWindow()
